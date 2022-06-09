0fd17091c7a8c8c68a388db9b5ad60d140612006
davquipe <davidcondori99@gmail.com>
2022-06-08 19:48:12 -0500

    Building left sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,15 +1,65 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/' >
             <img src={LogoS} alt='logo' />
-            <img src={LogoSubtitle} alt='slobodan' />
+            <img className='sub-logo' src={LogoSubtitle} alt='slobodan' />
         </Link>
+        <nav>
+            <NavLink
+                exact="true"
+                activeclassname="active"
+                to="/"
+            >
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink 
+                exact="true"
+                activeclassname="active"
+                className='about-link'
+                to="/about"
+            >
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink
+                exact="true"
+                activeclassname="active"
+                className='contact-link'
+                to="/contact"
+            >
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/davquipe'>
+                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.github.com/davquipe'>
+                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.twitter.com/davquipe/'>
+                    <FontAwesomeIcon icon={faTwitter} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.instagram.com/davquipe/'>
+                    <FontAwesomeIcon icon={faInstagram} color='#4d4d4e' />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
